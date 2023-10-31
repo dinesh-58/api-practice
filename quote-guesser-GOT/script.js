3533000c3a25e3ebe3ff6782f864fdecd40a519c
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-10-31 12:21:20 +0545

    quote-guesser-GOT: finish functionality

--- quote-guesser-GOT/script.js
+++ quote-guesser-GOT/script.js
@@ -1,22 +1,23 @@
-// get random quote
-// https://api.gameofthronesquotes.xyz/v1/random
-
 let choices = null;
-// array of objects{name, slug}. sort by slug and place in document
 let correctChoice = null;
 
 
 fetch("https://api.gameofthronesquotes.xyz/v1/random/3")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         correctChoice = data[0];
         document.querySelector('blockquote').innerText = correctChoice.sentence;
 
         choices = data.map(x => x.character.name);
         choices.sort();
-        document.querySelectorAll('label.user-choice').forEach((x, i) => {
+        document.querySelectorAll('label.user-choice').forEach((x, i, thisArr) => {
             x.querySelector('span').innerText = choices[i];
+            x.onclick = () => {
+                // set green background if correct else red
+                thisArr.forEach((label) => 
+                    label.style.background = (label.querySelector('span').innerText == correctChoice.character.name) ? '#55ff55' : '#ff5555'
+                );
+            };
         });
     })
     .catch(err => console.error(err));
@@ -24,4 +25,4 @@ fetch("https://api.gameofthronesquotes.xyz/v1/random/3")
 document.querySelector('#btn-hint').onclick = () => {
     document.querySelector('#hint-text').innerText = 'Someone from ' + correctChoice.character.house.name;
 }
-// TODO: need to place choices randomly
+
